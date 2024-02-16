b9f8e17688c6e61e095947a4b08cbe27f96f635d
Lavine Oluoch <lavineoluoch@gmail.com>
2024-02-15 19:57:17 -0600

    Update hw1-mapper1.py

--- hw1-mapper1.py
+++ hw1-mapper1.py
@@ -14,7 +14,7 @@ for line in reader(sys.stdin):
     boro, crime = (line[13].strip(), line[7].strip()) # Loop through rows to get boro, crime vars
     if not boro or not crime or boro == "BORO_NM":
         continue
-    print(crime + " - " + boro)
+    print '%s\t%s' % (boro, 1)
      
 
     # rest of the code goes here ...

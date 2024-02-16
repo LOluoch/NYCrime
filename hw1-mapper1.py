f32af66a4ecf5872da26bfae183c6ac2b0ab7491
Lavine Oluoch <lavineoluoch@gmail.com>
2024-02-15 19:30:20 -0600

    Update hw1-mapper1.py

--- hw1-mapper1.py
+++ hw1-mapper1.py
@@ -10,7 +10,7 @@
 from csv import reader
 import sys
 
-for line in reader(sys.stdin)[:5]:
+for line in reader(sys.stdin):
     boro, crime = (line[13].strip(), line[7].strip()) # Loop through rows to get boro, crime vars
     if not boro or not crime or boro == "BORO_NM":
         continue

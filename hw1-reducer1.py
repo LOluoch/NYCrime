0ca2e2dbb10b1ea416c0836cde9b748f01dd1cc4
Lavine Oluoch <lavineoluoch@gmail.com>
2024-02-19 21:30:02 -0600

    Update hw1-reducer1.py

--- hw1-reducer1.py
+++ hw1-reducer1.py
@@ -12,11 +12,15 @@ import sys
 current_boro = None
 current_count = 0
 boro = None
+crime = None
 stats = {}
+stats2 = {}
 
 for line in sys.stdin:
     line = line.strip()
     boro, count = line.split('\t', 1)
+    boro, crime = line.split('\t')
+    print (crime)
     try:
       count = int(count)
     except ValueError:
@@ -24,6 +28,7 @@ for line in sys.stdin:
     if current_boro == boro:
       current_count += count
       stats[current_boro] = current_count
+      stats2[current_boro] = current_count
     else:
       if current_boro: 
         print ('%s\t%s' % (current_boro, current_count))

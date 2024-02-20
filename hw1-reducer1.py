3c9e1ee67dabe7541900e0f1534297198201822b
Lavine Oluoch <lavineoluoch@gmail.com>
2024-02-20 14:11:53 -0600

    Update hw1-reducer1.py

--- hw1-reducer1.py
+++ hw1-reducer1.py
@@ -1,11 +1,3 @@
-# Write a MapReduce program in Python 3 (hw1-mapper1.py and hw1-reducer1.py) that will answer the
-# following based on New York City Crime Data 2016. Run the program with only one reduce task.
-# a) Where is most of the crime happening in New York? (e.g BRONX, QUEENS, BROOKLYN, etc.)
-# b) What is the total number of crimes reported in that location ?
-# c) What types of crime are happening in that location (show unique crime types) ?
-
-
-
 #!/usr/bin/env python
 import sys
 
